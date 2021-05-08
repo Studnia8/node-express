fc5ef99fbd6344a7418aa20f59d2d046dba221f9
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-08 20:28:43 +0200

    master push

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     db: 'mongodb+srv://admin:WgjPhbQjjp2qxT3J@database.hwft8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     login: 'admin',
-    password: 'WgjPhbQjjp2qxT3J',
     keySession: ['KLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000, // 24 hours
 }
