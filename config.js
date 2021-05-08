2992c8d0e1b100ce2a030b357def0c21e23d3486
Michał Studnicki <michalstudnicki8@gmail.com>
2021-05-08 22:06:16 +0200

    config secret

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://admin:WgjPhbQjjp2qxT3J@database.hwft8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    login: 'admin',
     keySession: ['KLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000, // 24 hours
 }
